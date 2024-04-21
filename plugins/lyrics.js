f182dfcb464f1c5812058a8504c64266b64120fc
bobizabote <167640869+bobizabote@users.noreply.github.com>
2024-04-22 00:03:08 +0100

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -16,7 +16,7 @@ ${result.lyrics}
 
     } catch (error) {
         console.error('Error:', error);
-        conn.reply(m.chat, `وقعت مشكلة راسل نورالدين \ninstagram.com/noureddine_ouafy`, m);
+        conn.reply(m.chat, '  وقعت مشكلة عمران \ninstagram.com/Imran_Hlimi`, m);
     }
 }
 
