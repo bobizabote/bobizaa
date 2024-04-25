2df4184f240c75fb375f489db7908ac0de543c2a
bobizabote <167640869+bobizabote@users.noreply.github.com>
2024-04-25 23:02:03 +0100

    Update deletechat.js

--- plugins/deletechat.js
+++ plugins/deletechat.js
@@ -1,7 +1,7 @@
 async function handler(m, { conn }) {
 
   conn.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }] }, m.chat)
-  let a = await m.reply("تم حذف رسائل هذه المحاذثة بنجاح") 
+  let a = await m.reply("*تم حذف رسائل هذه المحاذتة بنجاح*") 
 
 }
 handler.help = ['deletechat'],
