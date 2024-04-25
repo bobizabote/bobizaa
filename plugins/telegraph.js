479a223c023162c52fa17a7a571aa59622ccc474
bobizabote <167640869+bobizabote@users.noreply.github.com>
2024-04-25 20:08:26 +0100

    Update telegraph.js

--- plugins/telegraph.js
+++ plugins/telegraph.js
@@ -4,14 +4,14 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'هذا الامر خاص برفع الصور لموقع\nhttps://telegra.ph/\nالان قم بالاشارة للصورة التي تريد رفعها للموقع وسوف تحصل على رابطها \n اشر اليها ثم اكتب\n*.telegraph*'
+  if (!mime) throw '*هذا الامر خاص برفع الصور لموقع*\nhttps://telegra.ph/\n*الان قم بالاشارة للصورة التي تريد رفعها للموقع وسوف تحصل على رابطها* \n *أشر اليها ثم اكتب*\n*.telegraph*'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   m.reply(`▢ ${media.length} Byte(s) 
 
 ▢ ${isTele ? '' : '(Unknown)'} 
-▢ *هذا هو رابط الصورة:* ${link}\n\n\nتابع صاحب البوت في حسابه\ninstagram.com/noureddine_ouafy
+▢ *هذا هو رابط الصورة:* ${link}\n\n\nتابع صاحب البوت في حسابه\ninstagram.com/hlimi_imran
   `)
 }
 handler.help = ['tourl']
